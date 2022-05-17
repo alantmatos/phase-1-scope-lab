c2f00f596bd6c2369523015acfeae9ab75889b72
Alan Torquatro de Matos <mr.alanmatos@hotmail.com>
2022-05-17 17:48:40 -0400

    commit tree 77bef7fc0fa8340176bc279c817230a0ce95d971

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+
+// test 1
+var customerName = 'bob';
+const leastFavoriteCustomer = 'initialValue';
+
+
+// test 2
+function upperCaseCustomerName(){    
+    customerName = customerName.toUpperCase()
+   }
+   
+
+ // test 3
+ function setBestCustomer() {    
+    bestCustomer = 'not bob';
+
+ }
+
+ // test 4
+function overwriteBestCustomer () {
+    bestCustomer = 'maybe bob'
+    
+}
+
+// test 5
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'newName';
+}
+
+changeLeastFavoriteCustomer()
